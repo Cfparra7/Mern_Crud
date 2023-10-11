9b795adca5474f3c8d3589ef49eada9c1e8c3c10
CristianParra <cfparra35@misena.edu.co>
2023-10-11 00:13:25 -0500

    cambio en las rutas y logout y login

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,7 +1,8 @@
 import User from '../models/user.model.js';
 import bcrypt from 'bcryptjs';
-import jwt from 'jsonwebtoken';
-import {token} from "morgan";
+import {createAccessToken} from '../libs/jwt.js'
+
+import {response} from "express";
 
 export const register = async (req, res) => {
     const {email, password, username} = req.body;
@@ -13,26 +14,45 @@ export const register = async (req, res) => {
             password: passwordHash, //#para encriptar la contraseña
         });
         const userSaved = await newUser.save(); // para que se pueda usar el await se tiene que usar el async de la linea 3 y tiene que estar en un try catch
-//#uso del json web token
-        jwt.sign(
-            {id: userSaved._id},
-            'secret123',
-            {expiresIn: "1d",},
-            (err, token) => {
-                if (err) console.log(err);
-                res.json({token});
-            }
-        );
-// se quita esta parte del codigo porque ya se manda el token
-  /*      res.json({
+        const token = await createAccessToken({id: userSaved._id});
+        res.cookie('token', token);
+        res.json({
             id: userSaved.id,
             username: userSaved.username,
             email: userSaved.email,
 
-        });*/
+        });
     } catch (error) {
         console.log(error);
 
     }
 };
-export const login = (req, res) => res.send('login');
+export const login = async (req, res) => {
+    const {email, password} = req.body;
+    try {
+        const userFound = await User.findOne({email})
+        if (!userFound) return res.status(400).json({message: "User not Fount"});
+
+        const isMatch = await bcrypt.compare(password, userFound.password);
+        if (!isMatch) return res.status(400).json({message: "Incorrect Password"})
+
+        const token = await createAccessToken({id: userFound._id});
+        res.cookie('token', token);
+        res.json({
+            id: userFound.id,
+            username: userFound.username,
+            email: userFound.email,
+
+        });
+    } catch (error) {
+        console.log(error);
+
+    }
+};
+
+export const logout =  (req, res) => {
+    res.cookie('token', "", {
+            expires: new Date(0),
+        })
+    return res.sendStatus(200);
+};
---
+++ src/libs/jwt.js
@@ -0,0 +1,17 @@
+//#uso del json web token
+import {TOKEN_SECRET} from '../config.js';
+import jwt from 'jsonwebtoken';
+
+export function createAccessToken(payload) {
+    return new Promise((resolve, reject) => {
+        jwt.sign(
+            payload,
+            TOKEN_SECRET,
+            {expiresIn: "1d",},
+            (err, token) => {
+                if (err) reject(err)
+                resolve(token)
+            }
+        );
+    });
+}
